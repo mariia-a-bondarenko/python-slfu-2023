ee7bb2621101da0df7730b4de0aa89a0e912d882
mariia-a-bondarenko <148889541+mariia-a-bondarenko@users.noreply.github.com>
2023-11-04 21:07:26 +0200

    Fix wrong comparison

--- 02-4.py
+++ 02-4.py
@@ -4,7 +4,7 @@ c = int(input("Введіть третє число: "))
 
 if (a < b and a >= c) or (a < c and a >= b):
     print(a)
-elif (b < a and b >= c) or (b < a and b >= c):
+elif (b < a and b >= c) or (b < c and b >= a):
     print(b)
 else:
     print(c)
